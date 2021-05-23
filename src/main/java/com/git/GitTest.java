7e5537b0940d99aac759dc82f31f4de888d2d68e
za1wang <zaiwangdoit@gmail.com>
2021-05-23 23:49:14 +0800

    hot-fix commit1

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git2222222!");
         System.out.println("hello git3333333!");
+        System.out.println("hot-fix!");
     }
 }
