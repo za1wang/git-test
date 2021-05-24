b5a0c2a5d0d9354da8d9edb20c733b9a2bfb2f09
za1wang <zaiwangdoit@gmail.com>
2021-05-24 23:30:43 +0800

    push commit 1

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix!");
         System.out.println("master 111!");
         System.out.println("hot-fix 111!");
+        System.out.println("push test!");
     }
 }
